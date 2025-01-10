949d686e9a93b9792dda761bff160837cea2c7d4
JonasCGN <jonasbo66@gmail.com>
2025-01-10 10:50:29 -0300

    Dijkstra finalizado, funcionando

--- Questao_3/src/main.c
+++ Questao_3/src/main.c
@@ -1,146 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <time.h>
 #include <math.h>
 
-#define QUANTIDADE_DE_VERTICES 3
-#define INFINITO_NEGATIVO INT_MIN
+#define QTD_VERTICE 10
+#define RAND_CAMINHO 101
 
-typedef struct stAresta
-{
-  double confiabilidade;
-} Aresta;
+typedef struct grafo{
+	int vertice[QTD_VERTICE];
+	float arestas[QTD_VERTICE][QTD_VERTICE];
+}Grafo;
 
-typedef struct stVertice
-{
-  int id;
-} Vertice;
+typedef struct {
+    int id;
+    float distancia;
+    int predecessor;
+} Distancia;
 
-typedef struct stGrafo
-{
-  Vertice vertices[QUANTIDADE_DE_VERTICES];
-  Aresta arestas[QUANTIDADE_DE_VERTICES][QUANTIDADE_DE_VERTICES];
-} Grafo;
-
-void inicializarGrafo(Grafo *grafo)
-{
-  for (int vertice_inicial = 0; vertice_inicial < QUANTIDADE_DE_VERTICES; vertice_inicial++)
-  {
-    grafo->vertices[vertice_inicial].id = vertice_inicial;
-    for (int vertice_final = 0; vertice_final < QUANTIDADE_DE_VERTICES; vertice_final++)
-      grafo->arestas[vertice_inicial][vertice_final].confiabilidade = 0;
-  }
+void exibirVertice(int id,Grafo grafo){
+	for(int i=0;i<QTD_VERTICE;i++){
+		printf("[%d] - [%d] = %f\n",id,i,grafo.arestas[id][i]);
+	}
 }
 
-void exibirGrafo(Grafo *grafo)
-{
-  for (int vertice_inicial = 0; vertice_inicial < QUANTIDADE_DE_VERTICES; vertice_inicial++)
-  {
-    printf("Vertice %d: \n", grafo->vertices[vertice_inicial].id);
-    for (int vertice_final = 0; vertice_final < QUANTIDADE_DE_VERTICES; vertice_final++)
-    {
-      printf("\tAresta [%d][%d]: " ,vertice_inicial, vertice_final);
-      printf("confibilidade %f\n", grafo->arestas[vertice_inicial][vertice_final].confiabilidade);
+void exibirGrafo(Grafo grafo){
+	for(int i=0;i<QTD_VERTICE;i++){
+		printf("Caminho %d\n", i);
+		exibirVertice(i,grafo);
+	}
+}
+
+void exibeInfoDistancia(Distancia distancia){
+	printf("Vertice atual: %d\n", distancia.id);
+	printf("Distancia: %f\n", distancia.distancia);
+	printf("Predecessor: %d\n", distancia.predecessor);
+}
+
+void exibeDistancia(Distancia *distancia){
+	for(int i=0;i<QTD_VERTICE;i++){
+		printf("Vertice: %d\n",i);
+		exibeInfoDistancia(distancia[i]);
+	}
+}
+
+void iniciarGrafo(Grafo *grafo){
+	for(int id=0;id<QTD_VERTICE;id++){
+		grafo->vertice[id] = id;
+		for(int aresta =0;aresta < QTD_VERTICE;aresta++)
+			grafo->arestas[id][aresta] = 0.0;
+	}
+}
+
+void criandoGrafo(Grafo *grafo){
+	for(int id=0;id<QTD_VERTICE;id++){
+		for(int aresta =id + 1;aresta < QTD_VERTICE;aresta++){
+			float confiabilidade = ((float)(rand() % RAND_CAMINHO) / 100);
+			grafo->arestas[id][aresta] = confiabilidade;
+			grafo->arestas[aresta][id] = confiabilidade;
+		}
+	}
+}
+
+void atualizaDistancia(Distancia *distancia, Grafo grafo, int destino_atual, int destino_pos) {
+    distancia[destino_pos].id = destino_pos;
+    distancia[destino_pos].predecessor = destino_atual;
+
+    if ((int)distancia[destino_pos].distancia == -2) {
+        distancia[destino_pos].distancia = grafo.arestas[destino_atual][destino_pos];
+    } else {
+        distancia[destino_pos].distancia = distancia[destino_atual].distancia * grafo.arestas[destino_atual][destino_pos];
     }
-  }
 }
 
-void popularGrafo(Grafo *grafo)
-{
-  for (int vertice_inicial = 0; vertice_inicial < QUANTIDADE_DE_VERTICES; vertice_inicial++)
-  {
-    for (int vertice_final = vertice_inicial + 1; vertice_final < QUANTIDADE_DE_VERTICES; vertice_final++)
-    {
-      double confiabilidade = (double)rand() / RAND_MAX;
-      grafo->arestas[vertice_inicial][vertice_final].confiabilidade = confiabilidade;
-      grafo->arestas[vertice_final][vertice_inicial].confiabilidade = confiabilidade;
+Distancia *dijkstra(int ini, Grafo grafo){
+	Distancia *distancia;
+	distancia = (Distancia*)malloc(QTD_VERTICE * sizeof(Distancia));
+
+	for (int i = 0; i < QTD_VERTICE; i++){
+		distancia[i].id = i;
+		distancia[i].distancia = -2;
+	}
+	distancia[ini].distancia = 1;
+
+	for(int destino_atual = ini; destino_atual < QTD_VERTICE;destino_atual++){
+		for(int destino_pos = 0;destino_pos < QTD_VERTICE;destino_pos++){
+
+			if(destino_atual != destino_pos){
+				float comp1 = distancia[destino_atual].distancia;
+				float comp2 = grafo.arestas[destino_atual][destino_pos];
+				float comp3 = distancia[destino_pos].distancia;
+
+				if(comp1 * comp2 > comp3){
+					atualizaDistancia(distancia,grafo,destino_atual,destino_pos);
+				}
+
+			}
+
+		}
+	}
+
+	return distancia;
+}
+
+void menorCaminho(int ini, int fim, Distancia *distancia) {
+    int caminho[QTD_VERTICE];
+    int pos = 0;
+    int atual = fim;
+
+    while (atual != ini && atual != -1) {
+        caminho[pos++] = atual;
+        atual = distancia[atual].predecessor;
     }
-  }
-}
-void djcastra(int inicio, int fim, Aresta arestas[QUANTIDADE_DE_VERTICES][QUANTIDADE_DE_VERTICES], double *distancias, int *predecessor)
-{
-  int visitados[QUANTIDADE_DE_VERTICES];
 
-  // Inicializa as distâncias, visitados e predecessores
-  for (int vertice_inicial = 0; vertice_inicial < QUANTIDADE_DE_VERTICES; vertice_inicial++)
-  {
-    distancias[vertice_inicial] = INFINITO_NEGATIVO;
-    visitados[vertice_inicial] = 0;
-    predecessor[vertice_inicial] = -1;
-  }
-  distancias[inicio] = 0;
+    if (atual == ini) {
+        caminho[pos++] = ini;
+    }
 
-  int vertice_maior_confianca;
-  int confiracao_vertice_atual = 0;
-  do 
-  {
-    vertice_maior_confianca = -1;
-    for (int vertice_atual = 0; vertice_atual < QUANTIDADE_DE_VERTICES - 1; vertice_atual++)
-      if (!visitados[vertice_atual] && (vertice_maior_confianca == -1 || distancias[vertice_atual] > distancias[vertice_maior_confianca]))
-        vertice_maior_confianca = vertice_atual;
-
-    if (!(vertice_maior_confianca == -1 || distancias[vertice_maior_confianca] == INFINITO_NEGATIVO))
-    {
-      visitados[vertice_maior_confianca] = 1;
-
-      for (int v = 0; v < QUANTIDADE_DE_VERTICES; v++)
-      {
-        double confiabilidade = arestas[vertice_maior_confianca][v].confiabilidade;
-
-        if (!visitados[v] && confiabilidade >= 0 &&
-            distancias[vertice_maior_confianca] != INFINITO_NEGATIVO &&
-            distancias[vertice_maior_confianca] + log(confiabilidade) > distancias[v])
-        {
-          distancias[v] = distancias[vertice_maior_confianca] + log(confiabilidade);
-          predecessor[v] = vertice_maior_confianca;
+    printf("Menor caminho de %d para %d: ", ini, fim);
+    for (int i = pos - 1; i >= 0; i--) {
+        printf("%d", caminho[i]);
+        if (i > 0) {
+            printf(" -> ");
         }
-      }
     }
-    confiracao_vertice_atual++;
-  } while (confiracao_vertice_atual < QUANTIDADE_DE_VERTICES - 1 && vertice_maior_confianca != -1);
-}
-
-void exibirCaminho(int inicio, int fim, int *predecessor)
-{
-  if (predecessor[fim] == -1)
-    printf("Não existe caminho entre %d e %d\n", inicio, fim);
-  else
-  {
-    int caminho[QUANTIDADE_DE_VERTICES];
-    int contador = 0;
-    int vertice_atual = fim;
-    while (vertice_atual != -1)
-    {
-      caminho[contador] = vertice_atual;
-      vertice_atual = predecessor[vertice_atual];
-      contador++;
-    }
-
-    printf("Caminho entre %d e %d: ", inicio, fim);
-    for (int indice = contador - 1; indice >= 0; indice--)
-      printf("%d ", caminho[indice]);
     printf("\n");
-  }
 }
 
-int main()
-{
-  srand((unsigned) 1);
-  // srand((unsigned) 2);
+int main(){
+	// srand((unsigned int)2);
 
-  Grafo grafo;
-  inicializarGrafo(&grafo);
-  popularGrafo(&grafo);
-  exibirGrafo(&grafo);
+	Grafo grafo;
+	Distancia *distancia;
+	int ini=0,fim=9;
 
-  double distancias[QUANTIDADE_DE_VERTICES];
-  int predecessor[QUANTIDADE_DE_VERTICES];
+	iniciarGrafo(&grafo);
+	criandoGrafo(&grafo);
+	exibirGrafo(grafo);
 
-  djcastra(0, 2, grafo.arestas, distancias, predecessor);
+	printf("Digite o numero de inicio:");
+	scanf("%d",&ini);
+	printf("Digite o numero de fim:");
+	scanf("%d",&fim);
 
-  exibirCaminho(0, 2, predecessor);
-  return 0;
+	distancia = dijkstra(ini,grafo);
+	
+	exibeDistancia(distancia);
+	menorCaminho(ini,fim,distancia);
+	
+	free(distancia);
+	return 0;
 }
